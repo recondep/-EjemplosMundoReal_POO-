e44bde1e8520357b552a10b2c21095105f6500c3
recondep <164689950+recondep@users.noreply.github.com>
2024-06-23 16:41:01 -0500

    Ejemplo2Objetospoo.py
    Tareasemana4Ejemplo2.py

---
+++ Ejemplo2Objetospoo.py
@@ -0,0 +1,12 @@
+class Libro:
+    def __init__(self, titulo, autor, genero, precio):
+        self.titulo = titulo
+        self.autor = autor
+        self.genero = genero
+        self.precio = precio
+
+    def mostrar_informacion(self):
+        print(f"Título: {self.titulo}")
+        print(f"Autor: {self.autor}")
+        print(f"Género: {self.genero}")
+        print(f"Precio: {self.precio}")
